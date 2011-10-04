d69619deb4f8814124c1dfe3a4ed3c07bc55858d
Bryan White <bryan.white@elic.org>
2011-10-04 11:52:52 -0600

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # handle successful signing
+      sign_in user
+      redirect_to user
     end
   end
   
